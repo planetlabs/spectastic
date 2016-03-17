31132a719833b05098fd319df0be887607c3e2b8
jacobstr <jacobstr@gmail.com>
2016-03-17 15:58:53 -0700

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='spectastic',
-    version='0.1.2',
+    version='0.2.0',
     description="Request and response validation compatible with swagger.",
     long_description=readme + '\n\n' + history,
     author="Jacob Straszynski",
--- spectastic/__init__.py
+++ spectastic/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Jacob Straszynski'
 __email__ = 'jacob.straszynski@planet.com'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 # flake8: noqa
 from .operation import (
