5972b950851763d0afe774b9d4c8ec8bb64b7884
jacobstr <jacobstr@gmail.com>
2016-03-24 18:11:26 -0700

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='spectastic',
-    version='0.2.2',
+    version='0.2.3',
     description="Request and response validation compatible with swagger.",
     long_description=readme + '\n\n' + history,
     author="Jacob Straszynski",
